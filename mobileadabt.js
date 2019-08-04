a22c3b62c644fa68f5a0301b2b0a6ae5cc3ce8ab
cevahireminonu <52790494+cevahireminonu@users.noreply.github.com>
2019-08-04 21:27:07 +0300

    Update mobileadabt.js

--- mobileadabt.js
+++ mobileadabt.js
@@ -1,13 +1,4 @@
-alert("Test");
-
-var meta = document.createElement('meta');
-meta.name = 'viewport';
-meta.content = 'width=device-width, initial-scale=1';
-document.getElementsByTagName('head')[0].appendChild(meta);
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
 $(document).ready(function() {
  $("body").CSS("width", "400px");
+Albert("test");
 });
